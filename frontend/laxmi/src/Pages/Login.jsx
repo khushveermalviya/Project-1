eaad56d9acfc800413f957ad1b40fa3eca105ac3
khushveer <khushveer@Khushveer>
2024-01-12 08:46:03 +0530

    add backend

---
+++ frontend/laxmi/src/Pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+<div  className='w-full h-full flex justify-center align-cente'>
+    <h1 className=' text-6xl text-center'>कार्य प्रगति पर है</h1>
+</div>
+
+
+    </div>
+  )
+}
+
+export default Login
---
+++ frontend/laxmi/src/Pages/Root.jsx
@@ -0,0 +1,51 @@
+import React, { useState,useEffect} from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import SIdeMenu from './SIdeMenu';
+import Login from './Login';
+import Home from './Home';
+import Nav from './Nav';
+import Cards from './Cards';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import Data from './Data';
+function Root() {
+  const [items, setItems] = useState(0)
+  function addCart(){
+      setItems(prev=>prev+1)
+  }
+
+  const [show, setShow] = useState(false);
+
+  const slide = () => {
+    setShow(!show);
+  } 
+
+  return (
+    <div>
+      <BrowserRouter>
+      <div className='flex justify-between ali w-full h-53 bg-blue-400 fixed top-0 left-0 right-0 z-20'>
+            <h1 className='text-lg'>laxmi keys</h1>
+            <div className='border-spacing-1 border-red-900  curser:pointer'>
+              <Link className='pr-5' to="/About">About</Link>
+              <Link className='pr-5' onClick={slide} to={show ? '/' : '/SIdeMenu'}>SideMenu</Link>
+            </div>
+            <div>
+              <Link className='pr-5' onClick={slide} to={show ? '/': "/Login"}>
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Login</button>
+             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>{items}</button>
+              </Link>
+            </div>
+   </div> 
+5
+        <Routes>
+          <Route path="/" element={<Cards addCart={addCart}  />} />
+          { show && <Route path="/SIdeMenu" element={<SIdeMenu />} />}
+          { show && <Route path="/Login" element={<Login />} />}
+        </Routes>
+      </BrowserRouter>
+      <div className='flex bg-red'></div>
+    </div>
+  );
+}
+
+export default Root;
