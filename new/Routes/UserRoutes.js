a3abd54664c17aabf26d271bb958189070b249bb
khushveer <khushveermalviya26@gmail.com>
2024-04-08 23:59:17 +0530

    stage2

--- new/Routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-import experess from "express"
-import UserControl  from "../controller/UserControl.js"
-import UserLogin from "../controller/UserLogin.js"
-
-const UserRoutes=experess.Router()
-UserRoutes.post("/api/Registration",UserControl)
-UserRoutes.post("/api/login",UserLogin)
-export default UserRoutes;
